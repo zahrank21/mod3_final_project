8e6b9ca4a757bafd68551582669d7e333949a493
Zahran Kabir <zahran@Zahrans-MacBook-Pro.local>
2018-03-27 12:52:25 -0400

    fixed users error

--- frontend/users.js
+++ frontend/users.js
@@ -1,29 +1,24 @@
 
 document.addEventListener("DOMContentLoaded", event => {
 
-let users = document.getElementById("users");
+// let users = document.getElementById("users");
 
 
 (function getUsers(){
   return fetch(BASE_URL + "users")
   .then(res => res.json())
-  .then(json => displayUsers(json))
 })()
 
-// function displayUsers(json) {
-//   json.forEach(user) {
-//     let p = document.createElement("p");
-//     p.innerText = user.name;
-//     users.innerHTML += p;
-//   }
-// }
 
-displayUsers = (json) => {
-    json.forEach(user => {
-    let p = document.createElement("p");
-    p.innerText = user["name"];
-    users.appendChild(p);
-  })
-}
+
+// displayUsers = (json) => {
+//
+//     json.forEach(user => {
+//       console.log(user)
+//     let p = document.createElement("p");
+//     p.innerText = user["name"];
+//     users.append(p);
+//   })
+// }
 
 })
