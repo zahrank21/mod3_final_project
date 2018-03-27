e45b626d9e47a2febc36ca7fb32ebf9f50b46bcc
github username <github email address>
2018-03-27 12:40:36 -0400

    splash page done-ish

--- frontend/index.js
+++ frontend/index.js
@@ -4,27 +4,22 @@ document.addEventListener("DOMContentLoaded", event => {
 
 let users = document.getElementById("users");
 
+  (function getUsers(){
+    return fetch(BASE_URL + "users")
+    .then(res => res.json())
+    .then(json => displayUsers(json))
+  })()
 
-(function getUsers(){
-  return fetch(BASE_URL + "users")
-  .then(res => res.json())
-  .then(json => displayUsers(json))
-})()
+  displayUsers = (json) => {
+      json.forEach(user => {
+      let p = document.createElement("p");
+      p.innerText = user["name"];
+      users.appendChild(p);
+    })
+  }
 
-// function displayUsers(json) {
-//   json.forEach(user) {
-//     let p = document.createElement("p");
-//     p.innerText = user.name;
-//     users.innerHTML += p;
-//   }
-// }
-
-displayUsers = (json) => {
-    json.forEach(user => {
-    let p = document.createElement("p");
-    p.innerText = user["name"];
-    users.appendChild(p);
-  })
-}
+  $('.menu .item')
+  .tab()
+  ;
 
 })
