6672e8ebaa1a6143e636fb70793061d31bb358de
github username <github email address>
2018-03-29 13:28:52 -0400

    MTA WORKING

--- frontend/login_signup.js
+++ frontend/login_signup.js
@@ -3,19 +3,19 @@ document.addEventListener("DOMContentLoaded", event => {
   $('.menu .item').tab();
   $('.ui.checkbox').checkbox('attach events', '.check.button', 'check');
 
-  // let splashPage = document.getElementById("main_body");
-  // let loginButton = document.getElementById("login_button");
-  // let signupButton = document.getElementById("signup_button");
-  //
-  // clearHTML = () => splashPage.innerHTML = "";
-  // loginButton.addEventListener("click", () => {
-  //   clearHTML();
-  //   // loadMainPage();
-  // })
-  // signupButton.addEventListener("click", () => {
-  //   clearHTML();
-  //   // loadMainPage();
-  // })
+  let splashPage = document.getElementById("toClear");
+  let loginButton = document.getElementById("login_button");
+  let signupButton = document.getElementById("signup_button");
+
+  clearHTML = () => splashPage.innerHTML = "";
+  loginButton.addEventListener("click", () => {
+    clearHTML();
+    // loadMainPage();
+  })
+  signupButton.addEventListener("click", () => {
+    clearHTML();
+    // loadMainPage();
+  })
 
 //endofdom
 })
